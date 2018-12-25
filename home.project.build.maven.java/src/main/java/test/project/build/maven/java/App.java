ca0a6cb4d0904f65495f778bf6e32bc5ea45fe5b
Andrey Ruban <16754503+Ar0x13@users.noreply.github.com>
2018-12-25 19:20:38 +0200

    Added hostname and IP address output

--- home.project.build.maven.java/src/main/java/test/project/build/maven/java/App.java
+++ home.project.build.maven.java/src/main/java/test/project/build/maven/java/App.java
@@ -4,10 +4,27 @@ package test.project.build.maven.java;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class App {
+    public static void main( String[] args ) { 
+        try {
+            System.out.println( "Hello World!" );    
+
+            // Get IP address
+            InetAddress ip = InetAddress.getLocalHost();
+            System.out.println("Your current IP address : " + ip);
+
+            // Get Hostname
+            InetAddress myHost = InetAddress.getLocalHost();
+            System.out.println("Hostname: " +  myHost.getHostName());
+        }
+
+        catch (UnknownHostException ex) {
+            ex.printStackTrace();
+        }
+    }   
 }
+
