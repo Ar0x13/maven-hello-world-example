7af6313414dbc58b12fe2f86d34488294b03e17d
Andrey Ruban <16754503+Ar0x13@users.noreply.github.com>
2018-12-25 19:37:46 +0200

    Update App.java

--- home.project.build.maven.java/src/main/java/test/project/build/maven/java/App.java
+++ home.project.build.maven.java/src/main/java/test/project/build/maven/java/App.java
@@ -4,27 +4,30 @@ package test.project.build.maven.java;
  * Hello world!
  *
  */
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class App {
-    public static void main( String[] args ) { 
+    public static void main( String[] args ) {
+        InetAddress ip;
+        String hostname;
+
         try {
-            System.out.println( "Hello World!" );    
+            System.out.println( "Hello World!" );
 
             // Get IP address
-            InetAddress ip = InetAddress.getLocalHost();
+            ip = InetAddress.getLocalHost();
             System.out.println("Your current IP address : " + ip);
 
             // Get Hostname
-            InetAddress myHost = InetAddress.getLocalHost();
-            System.out.println("Hostname: " +  myHost.getHostName());
+            hostname = ip.getHostName();
+            System.out.println("Hostname: " +  hostname);
         }
-
+        
         catch (UnknownHostException ex) {
             ex.printStackTrace();
         }
-    }   
+    }
 }
+
 
